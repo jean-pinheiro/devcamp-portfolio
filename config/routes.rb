fc287a571f62cb8e238ee566ff21ac9832d551a1
Jean Pinheiro <jean.carlo.pc@gmail.com>
2018-11-29 14:49:39 -0300

    Customized porfolios routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :porfolios
+  resources :porfolios, except: [:show]
+  get 'porfolio/:id', to: 'porfolios#show', as: 'porfolio_show'
   get 'pages/home'
   get 'about-me', to: 'pages#about'
   get 'contact', to:'pages#contact'
