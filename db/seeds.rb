978d42149e9f05e01e00a15bb772f5e820502e7c
Jean Pinheiro <jean.carlo.pc@gmail.com>
2018-11-29 10:41:22 -0300

    Created seeds files for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post #{blog}",
+		body: "Lorem Ipsum is simply dummy text of the printing and
+		  typesetting industry. Lorem Ipsum has been the industry's
+		  standard dummy text ever since the 1500s, when an unknown
+		  printer took a galley of type and scrambled it to make a
+		  type specimen book. It has survived not only five centuries,
+		  but also the leap into electronic typesetting, remaining 
+		  essentially unchanged. It was popularised in the 1960s
+		  with the release of Letraset "
+	)
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 15
+	)
+end
+
+puts "5 skills created"
+
+9.times do |porfolio_item|
+	Porfolio.create!(
+		title: "Portfolio title: #{porfolio_item}",
+		subtitle: "The service",
+		body:"Lorem Ipsum is simply dummy text of the printing and
+		  typesetting industry. Lorem Ipsum has been the industry's
+		  standard dummy text ever since the 1500s",
+		main_image:"https://via.placeholder.com/600x400",
+		thumb_image:"https://via.placeholder.com/350x200",
+	)
+end
+
+puts "9 porfolio items created"
